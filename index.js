23ef2537c464353c9a5afa0d45a6305ae3367477
rohit <rohitrambade18@gmail.com>
2023-01-25 20:09:29 +0530

    added 2 Model

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/tasks")
   .then(() => console.log("Database is Connected"))
   .catch((err) => console.log(err));
+
+const User = require("./model/User");
+const Task = require("./model/Task");
---
+++ model/Task.js
@@ -0,0 +1,8 @@
+const taskSchema = new mongoose.Schema({
+  description: String,
+  isCompleted: Boolean,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
