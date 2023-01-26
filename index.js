b07c84292426588d65900230bb7374942decd26d
rohit <rohitrambade18@gmail.com>
2023-01-26 18:02:50 +0530

    express , storing user & task in DB

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 mongoose
   .connect("mongodb://127.0.0.1:27017/tasks")
@@ -6,28 +9,56 @@ mongoose
 
 const User = require("./model/User");
 const Task = require("./model/Task");
+app.use(express.json());
 
-async function db() {
+app.post("/task", async (req, res) => {
   try {
-    const user = new User({
-      name: "test",
-      age: 23,
-      email: "test@gmail.com",
-      password: "rohit@121",
-    });
+    const task = new Task(req.body);
+    console.log(task);
 
-    const task = new Task({
-      description: "task",
-      isCompleted: true,
-    });
+    await task.save();
+    return res.status(201).json({ success: true, task });
+  } catch (e) {
+    return res.status(400).json({ success: false, message: e.message });
+  }
+});
+
+app.post("/user", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    console.log(user);
 
     await user.save();
-    await task.save();
-
-    console.log(task);
+    return res.status(201).json({ success: true, user });
   } catch (e) {
-    console.log(e.message);
+    return res.status(400).json({ success: false, message: e.message });
   }
-}
+});
 
-db();
+// async function db() {
+//   try {
+//     const user = new User({
+//       name: "test",
+//       age: 23,
+//       email: "test@gmail.com",
+//       password: "rohit@121",
+//     });
+
+//     const task = new Task({
+//       description: "task",
+//       isCompleted: true,
+//     });
+
+//     await user.save();
+//     await task.save();
+
+//     console.log(task);
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// }
+
+// db();
+app.listen(port, () => {
+  console.log(`Server Is Running At Port ${port}`);
+});
